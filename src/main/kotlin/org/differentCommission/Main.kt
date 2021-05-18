627a26f94c41b529d7b9c5af2444f2b3927ecae1
Sergey Fortis <s_fortis@mail.ru>
2021-05-18 23:43:27 +0500

    Доработал Комиссию

--- src/main/kotlin/org/differentCommission/Main.kt
+++ src/main/kotlin/org/differentCommission/Main.kt
@@ -1,25 +1,25 @@
 package org.differentCommission
 
 fun main() {
-    println(commission(30000,"5000", "Maestro"))
-
+    print(commission(30000, 60000, "Maestro"))
 }
 
+fun commission(inputNewTransfer: Int, inputOldTransfer: Int = 0, inputCard: String = "VK"): Any =
+    when (inputCard) {
 
-fun commission(inputNewTransfer: Int, inputOldTransfer: String? = "0", inputCard: String? = "VK"): String? {
-
-    return when{
-        inputCard == "VK" && inputNewTransfer >15000 ->"Сумму разового перевода превышает 15000 рублей. Операция отклонена!"
-        inputCard == "VK" && (inputNewTransfer + Integer.parseInt(inputOldTransfer)) > 45000 ->"Общая сумма перевода за месяц превышает 45000 рублей. Операция отклонена!"
-        (inputCard == "Master" || inputCard == "Maestro" || inputCard == "Мир" || inputCard == "Visa" ) && (Integer.parseInt(inputOldTransfer) + inputNewTransfer > 600000) -> {
-                println("Общий размер суммы перевода за месяц превышает 600 000 рублей.").toString()
-
+        "Master", "Maestro" -> {
+            if (75000 < (inputOldTransfer + inputNewTransfer) && (inputOldTransfer + inputNewTransfer) < 600000) {
+                ((inputNewTransfer / 100 * 0.6 + 20) * 100).toInt()
+            } else 0
         }
-        (inputCard == "Master" || inputCard == "Maestro") && (75000 < Integer.parseInt(inputOldTransfer) + inputNewTransfer &&  Integer.parseInt(inputOldTransfer) + inputNewTransfer < 600000) -> String.format("%.0f",(inputNewTransfer/100*0.6 + 20)*100)
-        (inputCard == "Visa" || inputCard == "Мир") && inputNewTransfer/100*0.75 < 35 ->"Комиссия составляет " + 35*100 + " копеек"
-        (inputCard == "Visa" || inputCard == "Мир") && inputNewTransfer/100*0.75 > 35 -> "Комиссия составляет " + String.format("%.0f",inputNewTransfer/100*0.75*100) + " копеек"
-
-        else -> "Комиссия составляет 0 копеек. "
+        "Visa", "Мир" -> {
+            if (inputNewTransfer / 100 * 0.75 < 35) {
+                35 * 100
+            } else {
+                (inputNewTransfer / 100 * 0.75 * 100).toInt()
+            }
+        }
+        else -> 0
     }
-}
+
 
